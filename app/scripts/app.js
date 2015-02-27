230790330097b418823a12b1592930e30a5d9597
merlos <merlos@gmail.com>
2015-02-27 16:21:44 +0100

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name panatransWebApp
+ * @description
+ * # panatransWebApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('panatransWebApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      
+      .when('/api', {
+        templateUrl: 'views/api.html',
+        controller: 'ApiCtrl'
+      })
+      
+      
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/api.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name panatransWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the panatransWebApp
+ */
+angular.module('panatransWebApp')
+  .controller('ApiCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
