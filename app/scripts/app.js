95e01b6e4daffb1bced628a2e7a615731e93dd3c
merlos <merlos@gmail.com>
2015-04-09 00:54:49 +0200

    improved responsive navigation on mobile devices

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,15 @@
 'use strict';
 
+
+//hack. Make navbar toggle when clicked
+$('.navbar .navbar-link').click(function() {
+    var navbar_toggle = $('.navbar-toggle');
+    if (navbar_toggle.is(':visible')) {
+        navbar_toggle.trigger('click');
+    }
+});
+
+
 /**
  * @ngdoc overview
  * @name panatransWebApp
@@ -8,6 +18,8 @@
  *
  * Main module of the application.
  */
+
+
 angular
   .module('panatransWebApp', [
     'ngAnimate',
